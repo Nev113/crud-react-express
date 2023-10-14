3f721eae5780467bafd452e28de2e26ed87d251a
Nev113 <nevosangkane446@gmail.com>
2023-10-14 22:58:19 +0700

    adding search id routing

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,17 +10,18 @@ export const getBooksData = (req, res) => {
 
 export const addDataBook = (req, res) => {
     const {title, desc, cover} = req.body;
-    const q = 'INSERT INTO books (`title`, `description`, `cover`) VALUES (?)';
+    const q = 'INSERT INTO books (`title`, `description`, `cover`) VALUE (?)';
     db.query(q, [[title, desc, cover]], (err) => {
-        if (err) return res.json({msg: 'Data gagal ditambahkan'}).status(500);
+        if (err) return res.json({msg: 'Data gagal ditambahkan', err: err}).status(500);
         return res.json({msg: 'Data berhasil ditambahkan.'});
     });
 };
 
 export const getDataBookByQuery = (req, res) => {
-    const { id } = req.query;
-    const q = 'SELECT FROM books WHERE id LIKES ?';
-    db.query(q, [[id]], (err, data) => {
+    const { id } = req.params;
+    const q = `SELECT * FROM books WHERE id = ${id}`;
+    console.log(id)
+    db.query(q, (err, data) => {
         if (err) return res.json({msg: 'Id gagal ditemukan'}).status(404);
         return res.json(data);
     });
--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,12 @@ import express from 'express';
 import { addDataBook, getBooksData, getDataBookByQuery } from './config/routes.js';
 
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.json('Hello from backend');
 });
-
-app.get('/books', (req, res) => {
-    const { id } = req.params;
-    if ( id ) {
-      return getDataBookByQuery(req, res);
-    }
-    return getBooksData(req, res);
-});
+app.get('/books', getBooksData);
+app.get('/books/:id', getDataBookByQuery);
 app.post('/books', addDataBook);
 
 app.listen(8800, () => {
